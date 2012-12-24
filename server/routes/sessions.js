8d3215b5dbd9bc7b56ea5e1518cf73bdf976a105
goferito <goferito@gmail.com>
2012-12-24 02:53:35 +0100

    Show saved sessions at /sessions

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,12 +1,11 @@
 var Session = require('../model/session').model;
 
 exports.showAll = function(req, res){
-    res.send("Sessions are coming...");
     Session.find()
             .exec(function(err, data){
                 if(err) console.log(err);
                 else{
-                    //console.log(data);
+                    res.send(data);
                     for(var i=0; i<data.length; i++){
                        console.log(data[i].up + data[i].down);
                        res.send(data[i].up + data[i].down);
