fa8736f00a67f1f6b1130a4c977aaead3fa68595
goferito <goferito@gmail.com>
2012-12-26 04:57:49 +0100

    Small bug fixed

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Session = mongoose.model('Session');
 
-exports.loadAll = function(req, res){
+exports.showAll = function(req, res){
     Session.find({user: req.session.user})
             .exec(function(err, data){
                 if(err) console.log(err);
