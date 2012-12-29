a5a159119a416013647ab1df8bbc78c981b1b849
goferito <goferito@gmail.com>
2012-12-29 12:22:56 +0100

    Save sessions in mongo

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var MongoStore = require('connect-mongo')(express);
 var mongoose = require('mongoose');
 var config = require('./config');
 
@@ -19,7 +20,12 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.cookieParser('very secret: blabla'));
-  app.use(express.session());
+  app.use(express.session({
+      secret: 'very secret 2: blablabla',
+      store: new MongoStore({
+          db: 'test',
+          collection: 'cookieSessions'})
+  }));
   app.use(express.methodOverride());
   app.use(function(req,res,next){
     if(req.session.err) res.locals.err = req.session.err;
@@ -46,7 +52,10 @@ app.post('/login', users.authenticate);
 /* Restricted to user */
 app.get('/', users.restrict, users.showDashboard);
 app.get('/dashboard', users.restrict, users.showDashboard);
-app.get('/sessions', users.restrict, sessions.showAll);
+app.get('/sessions', 
+        users.restrict, 
+        sessions.loadAll,
+        sessions.showAll);
 
 
 http.createServer(app).listen(app.get('port'), function(){
