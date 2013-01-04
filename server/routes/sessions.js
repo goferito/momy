d6e011859908b75b8732d5e9368736011743ac58
goferito <goferito@gmail.com>
2013-01-05 00:18:55 +0100

    Hot times working

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -3,48 +3,64 @@ var sessions = require('../model/session');
 exports.showAll = function(req, res){
     res.locals.menu = 'sessions';
 
-    Date.prototype.getWeek = function() {
-        var onejan = new Date(this.getFullYear(),0,1);
-        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
-    } 
-
     var totalSecs = 0;
-    var weekTotals = [];
-    var dayTotals = [];
     var devices = [];
-    for(var j=0;j<52;j++) weekTotals[j]=0; // 52 weeks every year
+    var dayTotals = [];
     for(var j=0;j<7;j++) dayTotals[j]=0;   //  7 days per week
+    var hotTimes = [];
+    for(var j=0;j<7;j++){
+        hotTimes[j]=[];
+        for(var k=0; k<48; k++) //48 halfs of hours
+            hotTimes[j][k]=0;
+    }
+
     for(i in res.locals.sessions){
         session = res.locals.sessions[i];
 
         var up = new Date(session.up.substring(1,20));
         var down = new Date(session.down.substring(1,20));
-
+        var weekDayUp = (up.getDay()+6)%7;  //+6%7 to make weeks start on monday
+        var weekDayDown = (down.getDay()+6)%7;
         var sessionSecs = (down.getTime() - up.getTime())/1000;
+
         totalSecs += sessionSecs;
-        weekTotals[up.getWeek()] += sessionSecs+0;
-        dayTotals[(up.getDay()+6)%7] += sessionSecs+0;
-        //+6%7 to make weeks start on monday
+        dayTotals[weekDayUp] += sessionSecs+0;
+        
 
-        if(devices[session.device])
-            devices[session.device] += sessionSecs;
-        else
-            devices[session.device] = sessionSecs;
-
+        if(devices[session.device]) devices[session.device] += sessionSecs;
+        else devices[session.device] = sessionSecs;
 
         res.locals.sessions[i].time = humanizeTime(sessionSecs);
 
-    }
+        console.log(session);
+        var timeUp = up.getHours()*2 + Math.round(up.getMinutes()/30);
+        var timeDown = down.getHours()*2 + Math.round(down.getMinutes()/30);
+        console.log('weekDayUp:'+weekDayUp);
+        console.log('weekDayDown:'+weekDayDown);
+        console.log('timeUp:'+timeUp);
+        console.log('timeDown:'+timeDown);
+        for(var w = weekDayUp; w <= weekDayDown; w++){
+            if(w != weekDayUp) var h = 0;
+            else var h = timeUp;
+            while(h<48){
+                if(w == weekDayDown && h==timeDown)
+                    break;
+                else
+                    hotTimes[w][h]++;
+                h++;
+            }
+        }
 
-    var devs = [];
+    }
+    res.locals.hotTimes = JSON.stringify(hotTimes);
+    console.log(JSON.stringify(hotTimes));
+    res.locals.devices = [];
     for(a in devices){
         var percent = Math.round(devices[a]/totalSecs*100);
-        devs.push({'label': a + ' (' + percent + '%)',
+        res.locals.devices.push({'label': a + ' (' + percent + '%)',
                     'value': percent });
     }
-    res.locals.devices = devs;
     res.locals.totalSecs = humanizeTime(totalSecs);
-    res.locals.weekTotals = weekTotals;
     res.locals.dayTotals = dayTotals;
 
     res.render('sessions');
